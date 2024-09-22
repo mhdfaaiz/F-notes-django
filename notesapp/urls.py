c0f74d37429044f09fb65ac7ee0004d75c6102ed
Muhammed Fayis P <mhdfaaiz803@gmail.com>
2024-09-22 17:37:14 +0530

    on deployment

---
+++ notesapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path("notes/", views.notes, name="notes"),
+    path("notes/<slug:slug>/", views.note_detail, name="note-detail"),
+    path("notes-search/", views.search_notes, name='notes-search')
+]
+
+
+#endpoints:
+
+#GET_ALL_NOTES_and_CREATE_NEW_NOTE = "127.0.0.1:8000/notes/"
+#GET_SPECIFIED_NOTE = "127.0.0.1:8000/notes/note-slug"
